11a1ad442d1594afc0bdd177e7803e6a171c56b0
tjhv10 <achiyabennatan@gmail.com>
2024-04-15 14:27:48 +0300

    very good

--- app/lib/Script.dart
+++ app/lib/Script.dart
@@ -6,15 +6,48 @@ import 'package:flutter/services.dart' show rootBundle;
 
 class Script {
   // ignore: non_constant_identifier_names
-  static final List<String> booksH =["בראשית", "שמות", "ויקרא", "במדבר", "דברים", "יהושוע",
-            "שופטים", "שמואל א", "שמואל ב", "מלכים א", "מלכים ב", "ישעיה", "ירמיה", "יחזקאל", "הושע", "יואל", "עמוס",
-            "עובדיה", "יונה", "מיכה", "נחום", "חבקוק", "צפניה", "חגי", "זכריה", "מלאכי", "תהילים", "משלי", "איוב",
-            "שיר השירים", "רות", "איכה", "קהלת", "אסתר", "דניאל", "עזרא", "נחמיה", "דברי הימים א", "דברי הימים ב",
-            "מתי", "מרקוס", "לוקס", "יוחנן", "מעשי השליחים", "אל הרומים", "הראשונה אל הקורינתים", "השניה אל הקורינתים",
-            "אל הגלטים", "אל האפסים", "אל הפיליפים", "אל הקולוסים", "הראשונה אל התסלונים", "השניה אל התסלונים",
-            "הראשונה אל טימותיאוס", "השניה אל טימותיאוס", "אל טיטוס", "אל פילימון", "אל העברים", "אגרת יעקב", "הראשונה לכיפא",
-            "השניה לכיפא", "הראשונה ליוחנן", "השניה ליוחנן", "השלישית ליוחנן", "איגרת יהודה", "התגלות"];
-
+static final List<String> booksH =[
+  "בראשית", "שמות", "ויקרא", "במדבר", "דברים", "יהושוע",
+  "שופטים", "שמואל א", "שמואל ב", "מלכים א", "מלכים ב", "ישעיה", "ירמיה", "יחזקאל", "הושע", "יואל", "עמוס",
+  "עובדיה", "יונה", "מיכה", "נחום", "חבקוק", "צפניה", "חגי", "זכריה", "מלאכי", "תהילים", "משלי", "איוב",
+  "שיר השירים", "רות", "איכה", "קהלת", "אסתר", "דניאל", "עזרא", "נחמיה", "דברי הימים א", "דברי הימים ב",
+  "מתי", "מרקוס", "לוקס", "יוחנן", "מעשי השליחים", "אל הרומים", "הראשונה אל הקורינתים", "השניה אל הקורינתים",
+  "אל הגלטים", "אל האפסים", "אל הפיליפים", "אל הקולוסים", "הראשונה אל התסלונים", "השניה אל התסלונים",
+  "הראשונה אל טימותיאוס", "השניה אל טימותיאוס", "אל טיטוס", "אל פילימון", "אל העברים", "אגרת יעקב", "הראשונה לכיפא",
+  "השניה לכיפא", "הראשונה ליוחנן", "השניה ליוחנן", "השלישית ליוחנן", "איגרת יהודה", "התגלות"];
+static final Map<String, int> bookIndexMap = {
+  "בראשית": 65, "שמות": 64, "ויקרא": 63, "במדבר": 62, "דברים": 61,
+  "יהושוע": 60, "שופטים": 59, "שמואל א": 58, "שמואל ב": 57, "מלכים א": 56,
+  "מלכים ב": 55, "ישעיה": 54, "ירמיה": 53, "יחזקאל": 52, "הושע": 51,
+  "יואל": 50, "עמוס": 49, "עובדיה": 48, "יונה": 47, "מיכה": 46,
+  "נחום": 45, "חבקוק": 44, "צפניה": 43, "חגי": 42, "זכריה": 41,
+  "מלאכי": 40, "תהילים": 39, "משלי": 38, "איוב": 37, "שיר השירים": 36,
+  "רות": 35, "איכה": 34, "קהלת": 33, "אסתר": 32, "דניאל": 31,
+  "עזרא": 30, "נחמיה": 29, "דברי הימים א": 28, "דברי הימים ב": 27, 
+  "מתי": 26, "מרקוס": 25, "לוקס": 24, "יוחנן": 23, "מעשי השליחים": 22, 
+  "אל הרומים": 21, "הראשונה אל הקורינתים": 20, "השניה אל הקורינתים": 19,
+  "אל הגלטים": 18, "אל האפסים": 17, "אל הפיליפים": 16, "אל הקולוסים": 15,
+  "הראשונה אל התסלונים": 14, "השניה אל התסלונים": 13, "הראשונה אל טימותיאוס": 12,
+  "השניה אל טימותיאוס": 11, "אל טיטוס": 10, "אל פילימון": 9, "אל העברים": 8,
+  "אגרת יעקב": 7, "הראשונה לכיפא": 6, "השניה לכיפא": 5, "הראשונה ליוחנן": 4,
+  "השניה ליוחנן": 3, "השלישית ליוחנן": 2, "איגרת יהודה": 1, "התגלות": 0,
+};
+static final Map<String, int> bookIndexMapE = {
+  "Genesis": 65, "Exodus": 64, "Leviticus": 63, "Numbers": 62, "Deuteronomy": 61,
+  "Joshua": 60, "Judges": 59, "Ruth": 58, "1 Samuel": 57, "2 Samuel": 56,
+  "1 Kings": 55, "2 Kings": 54, "1 Chronicles": 53, "2 Chronicles": 52, "Ezra": 51,
+  "Nehemiah": 50, "Esther": 49, "Job": 48, "Psalm": 47, "Proverbs": 46,
+  "Ecclesiastes": 45, "Song of Solomon": 44, "Isaiah": 43, "Jeremiah": 42, "Lamentations": 41,
+  "Ezekiel": 40, "Daniel": 39, "Hosea": 38, "Joel": 37, "Amos": 36,
+  "Obadiah": 35, "Jonah": 34, "Micah": 33, "Nahum": 32, "Habakkuk": 31,
+  "Zephaniah": 30, "Haggai": 29, "Zechariah": 28, "Malachi": 27, "Matthew": 26,
+  "Mark": 25, "Luke": 24, "John": 23, "Acts": 22, "Romans": 21,
+  "1 Corinthians": 20, "2 Corinthians": 19, "Galatians": 18, "Ephesians": 17, "Philippians": 16,
+  "Colossians": 15, "1 Thessalonians": 14, "2 Thessalonians": 13, "1 Timothy": 12, "2 Timothy": 11,
+  "Titus": 10, "Philemon": 9, "Hebrews": 8, "James": 7, "1 Peter": 6,
+  "2 Peter": 5, "1 John": 4, "2 John": 3, "3 John": 2, "Jude": 1,
+  "Revelation": 0,
+};
   static final List<String> books = [
     "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", 
     "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", 
@@ -25,6 +58,23 @@ class Script {
     "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", 
     "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"
   ];
+  static final Map<String, String> bookDictionary = {
+  "בראשית": "Genesis", "שמות": "Exodus", "ויקרא": "Leviticus", "במדבר": "Numbers", "דברים": "Deuteronomy",
+  "יהושוע": "Joshua", "שופטים": "Judges", "שמואל א": "1 Samuel", "שמואל ב": "2 Samuel", "מלכים א": "1 Kings",
+  "מלכים ב": "2 Kings", "ישעיה": "Isaiah", "ירמיה": "Jeremiah", "יחזקאל": "Ezekiel", "הושע": "Hosea",
+  "יואל": "Joel", "עמוס": "Amos", "עובדיה": "Obadiah", "יונה": "Jonah", "מיכה": "Micah",
+  "נחום": "Nahum", "חבקוק": "Habakkuk", "צפניה": "Zephaniah", "חגי": "Haggai", "זכריה": "Zechariah",
+  "מלאכי": "Malachi", "תהילים": "Psalms", "משלי": "Proverbs", "איוב": "Job", "שיר השירים": "Song of Solomon",
+  "רות": "Ruth", "איכה": "Lamentations", "קהלת": "Ecclesiastes", "אסתר": "Esther", "דניאל": "Daniel",
+  "עזרא": "Ezra", "נחמיה": "Nehemiah", "דברי הימים א": "1 Chronicles", "דברי הימים ב": "2 Chronicles",
+  "מתי": "Matthew", "מרקוס": "Mark", "לוקס": "Luke", "יוחנן": "John", "מעשי השליחים": "Acts",
+  "אל הרומים": "Romans", "הראשונה אל הקורינתים": "1 Corinthians", "השניה אל הקורינתים": "2 Corinthians",
+  "אל הגלטים": "Galatians", "אל האפסים": "Ephesians", "אל הפיליפים": "Philippians", "אל הקולוסים": "Colossians",
+  "הראשונה אל התסלונים": "1 Thessalonians", "השניה אל התסלונים": "2 Thessalonians", "הראשונה אל טימותיאוס": "1 Timothy",
+  "השניה אל טימותיאוס": "2 Timothy", "אל טיטוס": "Titus", "אל פילימון": "Philemon", "אל העברים": "Hebrews",
+  "אגרת יעקב": "James", "הראשונה לכיפא": "1 Peter", "השניה לכיפא": "2 Peter", "הראשונה ליוחנן": "1 John",
+  "השניה ליוחנן": "2 John", "השלישית ליוחנן": "3 John", "איגרת יהודה": "Jude", "התגלות": "Revelation",
+};
   static String decodeUnicode(String encodedStr) {
     StringBuffer sb = StringBuffer();
     int i = 0;
@@ -89,6 +139,8 @@ class Script {
     List<String> results;
     DateTime startTime = DateTime.now();
     if (filePath == "assets/bible.txt") {
+      startBook = bookDictionary[startBook]!;
+      endBook = bookDictionary[endBook]!;
       results = await searchInBible(searchTerm, countWords(searchTerm), chosenPercent, ChoosenBooks(startBook,endBook,books), filePath);
     } else {
       results = await searchInBibleH(searchTerm, countWords(searchTerm), chosenPercent, ChoosenBooks(startBook,endBook,booksH), filePath);
@@ -133,12 +185,15 @@ class Script {
     catch (e) {
       print(e);
     }
-    results.sort((a, b) {
-    // Extract percentages from strings
-    int percentA = int.parse(a.split('אחוזי התאמה: ')[1].split(' ')[0]);
-    int percentB = int.parse(b.split('אחוזי התאמה: ')[1].split(' ')[0]);
-    // Compare percentages
-    return percentB.compareTo(percentA); // Sorting in descending order
+    
+    results.sort((a,b){
+      int placmentA = int.parse(a.split('אחוזי התאמה: ')[1].split(' ')[0])*1000000
+      + bookIndexMap[a.split('ספר: ')[1].split("פרק: ")[0].substring(0,a.split('ספר: ')[1].split("פרק: ")[0].length-1)]!*10000+
+      int.parse(b.split('פרק: ')[1].split(' ')[0])*-100 +int.parse(b.split('פסוק: ')[1].split(' ')[0])*-1;
+      int placmentB = int.parse(b.split('אחוזי התאמה: ')[1].split(' ')[0])*1000000
+      + bookIndexMap[b.split('ספר: ')[1].split("פרק: ")[0].substring(0,b.split('ספר: ')[1].split("פרק: ")[0].length-1)]!*10000+
+      int.parse(b.split('פרק: ')[1].split(' ')[0])*-100 +int.parse(b.split('פסוק: ')[1].split(' ')[0])*-1;
+      return placmentB.compareTo(placmentA);
     });
     return results;
   }
@@ -175,11 +230,42 @@ class Script {
       print(e);
     }
     results.sort((a, b) {
-    // Extract percentages from strings
-    int percentA = int.parse(a.split('Match percent: ')[1].split(' ')[0]);
-    int percentB = int.parse(b.split('Match percent: ')[1].split(' ')[0]);
-    // Compare percentages
-    return percentB.compareTo(percentA); // Sorting in descending order
+    // Parse matched percent from string 'a'
+int matchPercentA = int.parse(a.split('Match percent: ')[1].split(' ')[0]);
+
+// Parse book name from string 'a'
+String bookNameA = a.split('Book: ')[1].split("Chapter: ")[0].substring(0, a.split('Book: ')[1].split("Chapter: ")[0].length - 1);
+
+// Get the index of the book from the map and multiply it by 10,000
+int bookIndexA = bookIndexMapE[bookNameA]! * 10000;
+
+// Parse chapter number from string 'b'
+int chapterNumberA = int.parse(a.split('Chapter: ')[1].split(' ')[0]);
+
+// Parse verse number from string 'b'
+int verseNumberA = int.parse(a.split('Verse: ')[1].split(' ')[0]);
+
+// Combine all parts into 'placementA'
+int placementA = matchPercentA * 1000000 + bookIndexA + chapterNumberA * -100 + verseNumberA * -1;
+// Parse matched percent from string 'b'
+int matchPercentB = int.parse(b.split('Match percent: ')[1].split(' ')[0]);
+
+// Parse book name from string 'b'
+String bookNameB = b.split('Book: ')[1].split("Chapter: ")[0].substring(0, b.split('Book: ')[1].split("Chapter: ")[0].length - 1);
+
+// Get the index of the book from the map and multiply it by 10,000
+int bookIndexB = bookIndexMapE[bookNameB]! * 10000;
+
+// Parse chapter number from string 'b'
+int chapterNumberB = int.parse(b.split('Chapter: ')[1].split(' ')[0]);
+
+// Parse verse number from string 'b'
+int verseNumberB = int.parse(b.split('Verse: ')[1].split(' ')[0]);
+
+// Combine all parts into 'placementB'
+int placementB = matchPercentB * 1000000 + bookIndexB + chapterNumberB * -100 + verseNumberB * -1;
+
+      return placementB.compareTo(placementA);
     });
     return results;
   }
